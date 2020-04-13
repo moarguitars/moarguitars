6918097ef23bb9bc99cac8a3410aac6a550d6e9e
John Haynes <john.haynes@wegotpop.com>
2020-04-13 15:18:52 +0100

    Update mail_handler.php

--- mail_handler.php
+++ mail_handler.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "{{ site.email }}"; // this is your Email address
+    $to = "mail@johnhayn.es"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $guitar = $_POST['guitarmodel'];
